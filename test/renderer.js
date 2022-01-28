a28331ee8f66da5177de992f30603900a258b5b1
gaviin-h <59942890+gaviin-h@users.noreply.github.com>
2022-01-27 21:37:50 -0800

    both apis tested

--- test/renderer.js
+++ test/renderer.js
@@ -1,32 +1,33 @@
 const btn=document.getElementById('btn')
-const term=document.getElementById('search_term').value
+const term=document.getElementById('search_term')
 const article=document.getElementById('article')
-
+const bar=document.getElementById('bar')
+var present=false
 // generate a URL that will search by the given term
-function create_url(search_term) {
+function create_url(search_term) {//spaces curently mess with it
      return 'https://newsapi.org/v2/everything?q=' +
-            search_term +
-            '&from=2022-01-27&' +
+            search_term + 
+            '&from=2022-01-28&' +
             'sortBy=popularity&' +
             'apiKey=c86e67c82f1e44e29fb5dd30095fb55b'
 }
-// asyncronous function to make an API call to the generated URL
+// asyncronous function to make an API call to the generated URL (this should be generalized in the app to pull the json from both apis)
 async function make_request(search_term){
     var url=create_url(search_term) // ignore the lack on error handling
     var req=new Request(url)
     fetch(req).then(function(response) {
         response.json().then((data)=>{
             // pass json to function to distribute its contents to the div selected (in REACT this is much easier)
-            update_page(data.articles[0])
+            update_page(data.articles[0].title, data.articles[0].description)
         })
     })
 }
 // update the page with the article data 
-function update_page(response){
+function update_page(name_tag, description){
     var title=document.createElement('h2')
-    title.innerText=response.title
+    title.innerText=name_tag
     var content=document.createElement('p')
-    content.innerText=response.description
+    content.innerText=description
     article.appendChild(title)
     article.appendChild(content)
 }
@@ -35,8 +36,26 @@ async function tester(search){
     // pass json to function to distribute its contents to the div selected (in REACT this is much easier)
     update_page(top_hit)
 }
+var resp
+async function make_filter(filter){
+    var url='https://littlesis.org/api/entities/search?q='+filter
+    var req=new Request(url)
+    fetch(req).then(function(response){
+        response.json().then((r)=>{
+            var name=r.data[0].attributes.name
+            var desc=r.data[0].attributes.blurb
+            update_page(name, desc)
+        })
+    })
+}
 // listen for a click of the 'submi' button
-btn.addEventListener('click', (async) => {
+document.addEventListener('click', async function(e) {
     // will get a json object from the request
-    make_request(term)
+    if(e.target==btn){
+        make_request(term.value)
+    }
+    else if(e.target.id=='filter_btn'){
+        var filter_term=document.getElementById('filter').value
+        make_filter(filter_term)
+    }
 })
