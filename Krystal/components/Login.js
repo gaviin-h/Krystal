cc6de2303a0b99709c2eccba136385ba718db859
gaviin-h <59942890+gaviin-h@users.noreply.github.com>
2022-02-11 21:35:16 -0800

    Login functional

--- Krystal/components/Login.js
+++ Krystal/components/Login.js
@@ -1,17 +1,40 @@
 import { View, TextInput, Button } from 'react-native';
 import React, { useState } from 'react';
-import Style from '../Style'
+import {StyleSheet} from 'react-native'
 
-function Login({attemptLogin}){
+function Login({ navigation, attemptLogin }){
   const [ user, setUser ] = useState(null)
   const [ pass, setPass ] = useState(null)
-  
+  const Style = StyleSheet.create({
+    login_page: {
+      justifyContent: 'flex-start',
+      MarginBottom: '50%',
+    },
+    login_element: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+      borderColor: 'white',
+      borderRadius: 10,
+    },
+  })
   return(
     <View style={Style.login_page}>
-      <TextInput style={Style.login_element} placeholder='user@email.com' onChangeText={text => setUser(text)}/>
-      <TextInput style={Style.login_element} placeholder='password' onChangeText={text => setPass(text)}/>
+      <TextInput 
+        style={Style.login_element} 
+        placeholder='user@email.com' 
+        onChangeText={text => setUser(text)}/>
+      <TextInput style={Style.login_element} 
+        placeholder='password' 
+        onChangeText={text => setPass(text)} 
+        secureTextEntry={true}/>
 
-      <Button style={Style.login_button} onPress={attemptLogin(user, pass)} title='login'/>
+      <Button onPress={() => attemptLogin([user, pass])} title='login'/>
+      <Button 
+        color='grey' 
+        title='create account' 
+        onPress={() => navigation.navigate("createAccount")}/>
     </View>
   )
 }
