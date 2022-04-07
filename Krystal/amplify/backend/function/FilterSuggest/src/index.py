30a3c45a8a3a730ae77a2e78955a6bea3faee5cf
gaviin-h <59942890+gaviin-h@users.noreply.github.com>
2022-04-06 23:03:29 -0700

    small filter term improvements

--- Krystal/amplify/backend/function/FilterSuggest/src/index.py
+++ Krystal/amplify/backend/function/FilterSuggest/src/index.py
@@ -1,16 +1,20 @@
-import json
-import requests
-
 def handler(event, context):
-    url = ('https://littlesis.org/api/'+
-       'entities/search?q='+ event['query'] 
-    )
+    import requests
+    q=event['query']
+    TAGS=['anti-choice', 'anti-gender', 'anti-transgender', 'australia', 'boston', 'buffalo', 'coronavirus', 'corporate-mapping-project', 'defence', 'finance', 'florida', 'fracking', 'georgia', 'hindutva', 'iapd', 'icecontractor', 'louisiana', 'massachusetts', 'méxico', 'michigan', 'migrantjustice', 'nys', 'philly', 'PIC', 'prosecutor', 'puerto-rico', 'raleigh', 'real-estate', 'san-diego', 'seditioncaucus', 'st.-louis', 'tech', 'virginia']
+    tags=f'&tags={q}' if q in TAGS else ''
+    
+    url = 'https://littlesis.org/api/entities/search?q='+q+tags
+        
+    try: 
+        response = requests.get(url).json()
+        r=[]
+        for item in response['data']:
+            if item['attributes']['parent_id'] is None or len(r)==0:
+                r.append( {item['attributes']['name']: (item['attributes']['id'], item['attributes']['aliases'])} ) 
 
-    response = requests.get(url).json()
-    r=[]
-    for i in response['data']:
-        r.append({i['attributes']['name']: i['id']})
-
+    except Exception as e:
+        r=str(e)
     return {
         "content" : r
   }
