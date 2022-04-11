cd6a5ef7098c75df3c34f85575e9af1b260591fc
gaviin-h <59942890+gaviin-h@users.noreply.github.com>
2022-04-11 15:46:08 -0700

    init

---
+++ Krystal/components/FilterContainer.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { View, StyleSheet} from 'react-native';
+import FilterBubble from './FilterBubble'
+
+export default function FilterContainer({suggestResults}){
+  const [ currentFilters, setCurrentFilters ] = useState(null)
+  function addTerm(term){
+    setCurrentFilters(currentFilters+[term])
+    suggestResults=suggestResults.filter(cur => cur !== term)
+  }
+  function deleteTerm(term){
+    setCurrentFilters(currentFilters.filter(cur => cur !== term))
+  }
+  const Style = StyleSheet.create({
+    container: {
+      backgroundColor: 'grey',
+      width: 200,
+      height: 300,
+      boderRadius: 20
+    }
+  })
+  return (
+    <View style={Style.container}>
+      {suggestResults.map((term) => 
+      ( <FilterBubble 
+          term={term}
+          addTerm={addTerm}
+        /> ))}
+      {suggestResults.map((term) => 
+      ( <FilterBubble 
+          term={term}
+          addTerm={addTerm}
+          deleteTerm={deleteTerm}
+        /> ))}
+    </View>
+  )
+}
